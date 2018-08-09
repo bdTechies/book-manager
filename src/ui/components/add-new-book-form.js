46e8000323ff28fe03076e2786f6d2b04b5e238a
sh4hids <hello@shahid.pro>
2018-08-09 20:37:09 +0600

    :tada: Added form field

--- src/ui/components/add-new-book-form.js
+++ src/ui/components/add-new-book-form.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { Grid, Typography, Button } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  Button,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+} from '@material-ui/core';
 import { Container, PaddedPaper, CustomButton } from '../base-kits';
 
 class AddNewBookForm extends Component {
@@ -12,18 +22,113 @@ class AddNewBookForm extends Component {
       <Grid container spacing={16}>
         <Grid item xs={12}>
           <PaddedPaper square>
-            <Grid container direction="row">
+            <Grid container direction="row" spacing={32}>
               <Grid item xs={6}>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Assumenda ratione non adipisci, magni necessitatibus doloribus
-                  placeat minima quaerat quod explicabo! Cupiditate debitis
-                  labore excepturi consequatur voluptate velit perferendis
-                  tempora omnis.
-                </p>
+                <TextField
+                  id="title"
+                  label="Title"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="none"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="author"
+                  label="Author"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="normal"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="translator"
+                  label="Translator"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="normal"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="publisher"
+                  label="Publisher"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="normal"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="categories"
+                  label="Categories"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="normal"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="book-cover"
+                  label="Book cover"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="normal"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  id="book-description"
+                  label="Book description"
+                  // value={this.state.name}
+                  // onChange={this.handleChange('name')}
+                  margin="normal"
+                  multiline={true}
+                  rows={3}
+                  rowsMax={5}
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
               </Grid>
               <Grid item xs={6}>
-                <h1>Hello</h1>
+                <FormLabel component="label">Assign responsibility</FormLabel>
+                <RadioGroup
+                  aria-label="Reading Status"
+                  name="reading-status"
+                  // value={this.state.value}
+                  // onChange={this.handleChange}
+                >
+                  <FormControlLabel
+                    value="completed"
+                    control={<Radio />}
+                    label="Completed"
+                  />
+                  <FormControlLabel
+                    value="reading"
+                    control={<Radio />}
+                    label="Reading"
+                  />
+                  <FormControlLabel
+                    value="not-started"
+                    control={<Radio />}
+                    label="Not started"
+                  />
+                </RadioGroup>
               </Grid>
               <Grid item xs={12}>
                 <Container align="right">
