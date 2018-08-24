f84027aed246d7538f22024a3e5967dc4cd248bd
sh4hids <hello@shahid.pro>
2018-08-24 20:20:25 +0600

    :tada: :tada: Added import/export design

--- src/ui/base-kits/custom-button.js
+++ src/ui/base-kits/custom-button.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import { Button } from '@material-ui/core';
+import { space } from 'styled-system';
 
 const CustomButton = styled(Button)`
   && {
     margin-right: ${props => (props.mr ? props.mr + 'px' : '')};
     margin-left: ${props => (props.ml ? props.ml + 'px' : '')};
+    ${space};
   }
 `;
 
--- src/ui/pages/import-export-page.js
+++ src/ui/pages/import-export-page.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Container } from '../base-kits';
+import { CloudUploadIcon, CloudDownloadIcon } from 'mdi-react';
+import {
+  Container,
+  CustomGrid,
+  CustomTypography,
+  CustomButton,
+} from '../base-kits';
 import { MainMenu, TopMenuBar } from '../components';
 
 class ImportExportPage extends Component {
@@ -8,7 +14,35 @@ class ImportExportPage extends Component {
       <Container main>
         <MainMenu />
         <TopMenuBar pageTitle="Import/Export Data" />
-        <p>Import/Export Data</p>
+        <CustomGrid
+          container
+          direction="column"
+          alignItems="center"
+          justify="center"
+          customheight="100%"
+        >
+          <CustomButton
+            variant="contained"
+            color="default"
+            size="small"
+            pr={32}
+            pl={32}
+            mb={16}
+          >
+            <CloudDownloadIcon />
+            <CustomTypography ml={12}>Import Data (CSV)</CustomTypography>
+          </CustomButton>
+          <CustomButton
+            variant="contained"
+            color="default"
+            size="small"
+            pr={32}
+            pl={32}
+          >
+            <CloudUploadIcon />
+            <CustomTypography ml={12}>Export Data (CSV)</CustomTypography>
+          </CustomButton>
+        </CustomGrid>
       </Container>
     );
   }
