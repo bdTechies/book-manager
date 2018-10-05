9a5df0c951dc0d9755a41c461f4f7c517dc743ed
sh4hids <hello@shahid.pro>
2018-10-05 18:45:24 +0600

    Fixed main menu test

--- src/ui/components/__test__/main-menu.test.js
+++ src/ui/components/__test__/main-menu.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import allReducers from '../../../reducers';
 import MainMenu from '../main-menu';
 import { shallow, mount } from '../../../enzyme';
 
+const store = createStore(allReducers, applyMiddleware(thunk));
+
 describe('<MainMenu />', () => {
   it('renders 1 <MainMenu /> component', () => {
-    const component = shallow(<MainMenu />);
+    const component = shallow(<MainMenu store={store} />);
     expect(component).toHaveLength(1);
   });
 });
