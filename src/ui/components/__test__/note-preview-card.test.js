d18195c9552520162c2d2573de792912ea7f3ed9
sh4hids <hello@shahid.pro>
2018-10-26 18:16:18 +0600

    Fixed note preview test

--- src/ui/components/__test__/note-preview-card.test.js
+++ src/ui/components/__test__/note-preview-card.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import NotePreviewCard from '../note-preview-card';
 import { shallow, mount } from '../../../enzyme';
 
+const book = {
+  title: 'Test book',
+  author: 'John Doe',
+  note:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+};
+
 describe('<NotePreviewCard />', () => {
   it('renders 1 <NotePreviewCard /> component', () => {
-    const component = shallow(<NotePreviewCard />);
+    const component = shallow(<NotePreviewCard {...book} />);
     expect(component).toHaveLength(1);
   });
 });
